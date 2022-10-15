2b3a2f1e799ef279f5c2c20c89b6b20d4d8f59c6
김지영 <90201537+famo1245@users.noreply.github.com>
2022-10-15 12:14:10 +0900

    Delete MP04 directory

--- MP04/src/mp04/FrameWindow.java
+++
@@ -1,9 +0,0 @@
-package mp04;
-
-import javax.swing.*;
-import java.awt.event.WindowListener;
-
-public abstract class FrameWindow {
-
-    public abstract JPanel createPanel(int width, int height);
-}
