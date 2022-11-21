5f85aa6926f820e130e03224ac1a743c0d4053ab
Jiyeong Kim <90201537+famo1245@users.noreply.github.com>
2022-11-22 01:05:12 +0900

    Delete Design_pattern/MP07 directory

--- Design_pattern/MP07/src/mp07/Main.java
+++
@@ -1,9 +0,0 @@
-package mp07;
-
-public class Main {
-    private static final String MAIN_TITLE = "Main Window";
-
-    public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow(MAIN_TITLE);
-    }
-}
--- Design_pattern/MP07/src/mp07/State.java
+++
@@ -1,9 +0,0 @@
-package mp07;
-
-public interface State {
-    public void returnChanges();
-    public void selectBeverage();
-    public void addHundred();
-    public void addFiveHundred();
-    public void addThousand();
-}
